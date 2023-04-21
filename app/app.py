9790e4bb354dbef543c41ed56c28fa1fab5c5c5d
Eric Trautsch <erictrautsch@outlook.com>
2023-04-21 13:57:20 -0500

    Update App, DockerFile and Action to pass env varibles properly

--- app/app.py
+++ app/app.py
@@ -39,12 +39,12 @@ def get_tables(tables = ['Customer']) -> List[Dict]:
 
 @app.route('/')
 def index() -> str:
-    #result = get_tables()
+    result = get_tables()
     # print('RESULT HERE', result)
-    return 'hi'#render_template('table.html', list_tables = result)
+    return render_template('table.html', list_tables = result)
 
 if __name__ == '__main__':
-    # db_init()
+    db_init()
     app.run(host='0.0.0.0', port = os.environ.get('PORT'))
 
 
