7a5147859dbe1f45ab10b97ce46ab5783cdea9f5
Eric Trautsch <erictrautsch@outlook.com>
2023-04-24 13:22:55 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -66,7 +66,8 @@ def index() -> str:
     tables = get_tables(['Inventory', 'Outgoing', 'Incoming', 'Part', 'StorageArea', 'Customer', 'Supplier', 'Employee'])
     views = get_tables(['inventory_summary', 'customer_sales_summary'])
     queries = [execute_query('SELECT * FROM Customer', 'Customer Query', 'Select all the tuples from Customer table'),
-               execute_query('SELECT * FROM Supplier', 'Supplier Query', 'SELECT all tuples from Supplier Table!')
+               execute_query('SELECT * FROM Supplier', 'Supplier Query', 'SELECT all tuples from Supplier Table!'),
+               execute_query('''SELECT p.part_id,  p.description,  s.storage_area_id,  s.area,  s.location, i.quantity FROM Inventory i JOIN Part p ON i.part_id = p.part_id JOIN StorageArea s ON i.storage_area_id = s.storage_area_id WHERE p.part_id = 1;''', 'Find Part X Query','Where can I find part id X in the warehouse to pick and add to an outgoing shipment? (Using part_id 1 as an example)')
                ] 
     # print('RESULT HERE', result)
     return render_template('table.html', list_tables = tables, list_views = views, list_queries = queries)
